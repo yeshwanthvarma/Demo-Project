487d29a91bcfbb0988e1ed7833718f12e4a64e7c
yeshwanthvarma <varma2632k@gmail.com>
2024-05-23 02:22:18 -0500

    Added Repository interface and annotated with @Repository.

--- Demo Project/src/main/java/com/example/demo/repository/OrderRepository.java
+++ Demo Project/src/main/java/com/example/demo/repository/OrderRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// Changing class to interface and extending JpaRepository
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
+
--- Demo Project/src/main/java/com/example/demo/repository/ProductRepository.java
+++ Demo Project/src/main/java/com/example/demo/repository/ProductRepository.java
@@ -2,7 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-// Changing class to interface and extending JpaRepository
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
